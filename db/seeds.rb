73788f074f58972c9a940c0a8966ba8f5dbf543a
Jiana Zhu <jiana.zhu@gmail.com>
2021-03-31 18:33:41 +0800

    rails-yelp-mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+  r = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: %w[chinese italian japanese french belgian].sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+
+  puts "Created Restaurant #{r.name}🥘"
+end
